24a5d1df494030230fbe6a196b37b912bb37b9c5
Kirstin Burns <35663199+kirbur@users.noreply.github.com>
2019-06-03 13:31:42 -0400

    IIWA-39 Reviewed comments and implemented changes

--- src/frontend-admin/src/components/App/App.tsx
+++ src/frontend-admin/src/components/App/App.tsx
@@ -1,37 +1,18 @@
 import React from 'react';
 import { Route, NavLink, BrowserRouter as Router, Switch} from 'react-router-dom'; 
 
-
 // Styles
 import styles from './App.module.css';
 
 // Primary Component
 export const App: React.FC = () => {
   return (
-    /*<div className={styles.app}>
-      <header className={styles.appHeader}>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={styles.appLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-  </div>*/
     <div className={styles.app}>
       {/*header*/}
 
       <Router>
-      
         <div className={styles.navContainer}>
-          <div className={styles.space} />
-          
-          <nav>
+          <nav className={styles.navBar}>
           
             <h1>CQL</h1>
             
@@ -61,21 +42,20 @@ export const App: React.FC = () => {
               </NavLink>
 
               <NavLink className={styles.navTab} 
-                activeClassName={styles.active} 
-                id={styles.lastTab}
+                activeClassName={styles.active}
                 to="/departments">
                 <label>Departments</label>
               </NavLink>
             </div>
           </nav>
         </div>
+        
         <Switch>
           {/* <Route />'s go here */}
           
         </Switch>
-      
     </Router>
-    <footer>
+    <footer className={styles.appFooter}>
 
     </footer>
   </div>
