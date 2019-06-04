ddba14a7d3352e25bed81dfa40766ba19034b3dc
Kirstin Burns <35663199+kirbur@users.noreply.github.com>
2019-06-04 12:24:34 -0400

    IIWA-44 Gave the bar an onClick & changed cursor

--- src/frontend-admin/src/components/HorizontalBarChart/HorizontalBarChart.tsx
+++ src/frontend-admin/src/components/HorizontalBarChart/HorizontalBarChart.tsx
@@ -7,14 +7,15 @@ interface IProps {
     title: string,
     amount: number,
     outOf: number,
+    onClick: any 
 }
 
 export const HorizontalBarChart: React.FC<IProps> = (props) => {
-    const {title, amount, outOf} = props
+    const { title, amount, outOf, onClick } = props
     return (
         amount > outOf ? null : (
             <div className={styles.barContainer}>
-                <div className={styles.barMain}>
+                <div className={styles.barMain} onClick={onClick}>
                     {amount <= 0 ? <div className={styles.barTitle}>{title}</div> : (
                     <div className={styles.barFiller} style={{width: `${amount / outOf * 100}%`}}>
                         <div className={styles.barTitle}>{title}</div>
