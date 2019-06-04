40e554539ece3658a0a7f28023aa3c3531cd6f19
Kirstin Burns <35663199+kirbur@users.noreply.github.com>
2019-06-04 12:44:34 -0400

    IIWA-44 Fixed tests to have onClick

--- src/frontend-admin/src/components/HorizontalBarChart/HorizontalBarChart.test.tsx
+++ src/frontend-admin/src/components/HorizontalBarChart/HorizontalBarChart.test.tsx
@@ -4,12 +4,12 @@ import {HorizontalBarChart} from './HorizontalBarChart';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<HorizontalBarChart title={"Test"} amount={8} outOf={10} />, div);
+  ReactDOM.render(<HorizontalBarChart title={"Test"} amount={8} outOf={10} onClick={() => {}} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
 it('renders without crashing when amount > outOf', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<HorizontalBarChart title={"Test"} amount={8} outOf={4} />, div);
+  ReactDOM.render(<HorizontalBarChart title={"Test"} amount={8} outOf={4} onClick={() => {}}/>, div);
   ReactDOM.unmountComponentAtNode(div);
 })
