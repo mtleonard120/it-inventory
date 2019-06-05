09da811c3ce52822108b78c896c9bfcc2fa14ef3
Kirstin Burns <35663199+kirbur@users.noreply.github.com>
2019-06-05 09:08:36 -0400

    IIWA-44 Finished Group component

--- src/frontend-admin/src/components/Group/Group.tsx
+++ src/frontend-admin/src/components/Group/Group.tsx
@@ -1,21 +1,25 @@
 import React from 'react'
-import { string } from 'prop-types';
 import styles from './Group.module.css'
+import cx from 'classnames' //allows use of more than 1 classname
 
 //Types
 type JustifyType = 'start' | 'end' | 'between' | 'around' | 'even'
 type DirectionType = 'row' | 'column'
 
 interface IProps {
-    children: JSX.Element[],
+    children: JSX.Element[], 
     direction?: DirectionType,
     justify?: JustifyType,
     className?: string,
 }
 
-//Component for arranging groups of similar items with flexbox
-const Group: React.FC<IProps> = (props) => {
-    const { children, direction, justify, className } = props
-    //need to fix so that div can have multiple classnames
-    return <div className={className}>{children}</div>
+// Component for arranging groups of similar items with flexbox
+// have to pass at least 2 children to use
+export const Group: React.FC<IProps> = (props) => {
+    const { children, direction = 'row', justify = 'start', className } = props
+    return (
+        <div className={cx(styles[justify], styles[direction], styles.group, className)} >
+            {children}
+        </div>
+    )
 }
