c2a47baa8f7c8c5850cb68e5837f7a9a59ff486a
Kirstin Burns <35663199+kirbur@users.noreply.github.com>
2019-06-06 13:23:36 -0400

    IIWA-31 can pass a function to dropdown

--- src/frontend-admin/src/components/reusables/Dropdown/Dropdown.tsx
+++ src/frontend-admin/src/components/reusables/Dropdown/Dropdown.tsx
@@ -1,20 +1,15 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './Dropdown.module.css'
 import { DropdownList } from './DropdownList'
 //import title to put the button inside of the corner brackets
 
 interface IDropdownProps {
-    content: {name: string, item: any, }[]
+    content: {name: string, item?: any, onClick?: Function}[]
 }
 
 export const Dropdown: React.FC<IDropdownProps> = (props) => {
     const {content} = props
-    const [list] = useState(content)
-    const [title, setTitle] = useState(content[0].name)
-
-    const getIndex = () => {
-        return content.indexOf( content.filter(i => i.name===title)[0] );
-    }
+    const [selected, setSelected] = useState(content[0])
 
     return (
         <div className={styles.dropdownMain}>
@@ -23,7 +18,7 @@ export const Dropdown: React.FC<IDropdownProps> = (props) => {
                 <DropdownList 
                     triggerElement={({isOpen, toggle}) => (
                         <button onClick={toggle} className={styles.dropdownButton}>
-                            <div className={styles.dropdownTitle}>{title}
+                            <div className={styles.dropdownTitle}>{selected.name}
                                 <div className={styles.dropdownArrow}/>
                             </div>
                         </button>
@@ -31,8 +26,10 @@ export const Dropdown: React.FC<IDropdownProps> = (props) => {
 
                     choicesList={() => (
                         <ul className={styles.dropdownList}>
-                            {list.map(i => (
-                                <li className={styles.dropdownListItem} key={i.name} onClick={() => setTitle(i.name)}>
+                            {content.map(i => (
+                                <li className={styles.dropdownListItem} key={i.name} onClick={() => {
+                                    setSelected(i)
+                                    selected.onClick && selected.onClick()}}>
                                     <a href="#0">
                                         <div className={styles.dropdownItemLabel}>{i.name}</div>
                                     </a>
@@ -45,7 +42,7 @@ export const Dropdown: React.FC<IDropdownProps> = (props) => {
                 <div/>
                 
             </div>
-            <div className={styles.selected}>{content[getIndex()].item}</div>
+            <div className={styles.selected}>{selected.item}</div>
         </div>
     )
 }
