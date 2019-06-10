c4a88cf1dc05e04c94cbd4caedc3b3e30baa4665
AlexMcDowell <alexmichaelmcdowell@gmail.com>
2019-06-10 15:29:00 -0400

    -

--- src/frontend-admin/src/components/App/App.tsx
+++ src/frontend-admin/src/components/App/App.tsx
@@ -10,20 +10,7 @@ import { CostCard } from '../pages/Dashboard/CostCard/CostCard';
 export const App: React.FC = () => {
   return (
     <div className={styles.app}>
-      <header className={styles.appHeader}>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className={styles.appLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <CostCard data={[100, 200]} />
+      <CostCard data={[100,200]} />
     </div>
   );
 }
