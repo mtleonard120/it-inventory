7cf7a470483d92e4028b47c337441fa8d458f466
Charles Kornoelje <charleskornoelje@gmail.com>
2019-06-10 15:31:16 -0400

    Fix migration ID. How do you learn if you don't make mistakes?

--- src/Cql.InventoryManagement.DbMigrations/Migrations/2019/06/201906101513-update-field-DateBought.cs
+++ src/Cql.InventoryManagement.DbMigrations/Migrations/2019/06/201906101513-update-field-DateBought.cs
@@ -3,7 +3,7 @@ using System;
 
 namespace Cql.InventoryManagement.DbMigrations.Migrations._2019._06
 {
-    [Migration(201906101051)]
+    [Migration(201906101513)]
     public class _201906101513_update_field_DateBought : AutoReversingMigration
     {
         public override void Up()
