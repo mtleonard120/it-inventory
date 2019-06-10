8611c7a677364c5ec531785303cca4c38e3ad74e
Charles Kornoelje <charleskornoelje@gmail.com>
2019-06-10 15:27:13 -0400

    Update field DateBought on Program table.

---
+++ src/Cql.InventoryManagement.DbMigrations/Migrations/2019/06/201906101513-update-field-DateBought.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+using System;
+
+namespace Cql.InventoryManagement.DbMigrations.Migrations._2019._06
+{
+    [Migration(201906101051)]
+    public class _201906101513_update_field_DateBought : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Alter.Column("DateBought")
+                .OnTable("Program")
+                .AsDate()
+                .Nullable();
+        }
+    }
+}
+
