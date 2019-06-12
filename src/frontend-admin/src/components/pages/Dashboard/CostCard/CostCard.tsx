057bdb152d5051ad232259c11b12e6625f04f2d0
AlexMcDowell <alexmichaelmcdowell@gmail.com>
2019-06-12 10:56:57 -0400

    axios stuff going on the dashboard

--- src/frontend-admin/src/components/pages/Dashboard/CostCard/CostCard.tsx
+++ src/frontend-admin/src/components/pages/Dashboard/CostCard/CostCard.tsx
@@ -10,10 +10,10 @@ import styles from './CostCard.module.css'
 // Primary Component
 interface ICostCardProps {
     cardTitle: string
-    // data: {
-    //     programsCost: number
-    //     pluginsCost: number
-    // }
+    data: {
+        programsCost: number
+        pluginsCost: number
+    }
     icon: any
 }
 
@@ -21,26 +21,27 @@ export const CostCard = (props: ICostCardProps) => {
     const {
         cardTitle,
         icon,
-        // data: {programsCost, pluginsCost},
+        data: {programsCost, pluginsCost},
     } = props
 
-    const axios = new AxiosService('', '')
-    let x: {
-        programsCost: number
-        pluginsCost: number
-    } = {programsCost: 0, pluginsCost: 0}
-    const [val, setVal] = useState(x)
-    useEffect(() => {
-        axios.get('/API/cost/dashboard', setVal)
-    }, [setVal])
-    console.log(val)
+    //axios stuff for the dashboard again
+    // const axios = new AxiosService('', '')
+    // let x: {
+    //     programsCost: number
+    //     pluginsCost: number
+    // } = {programsCost: 0, pluginsCost: 0}
+    // const [val, setVal] = useState(x)
+    // useEffect(() => {
+    //     axios.get('/API/cost/dashboard', setVal)
+    // }, [setVal])
+    // console.log(val)
 
     return (
         <Card title={cardTitle}>
             <div>{icon}</div>
-            <h1 className={styles.title}>Total: ${val.programsCost + val.pluginsCost}</h1>
-            <h1 className={styles.title}>Programs: ${val.programsCost}</h1>
-            <h1 className={styles.title}>Plugins: ${val.pluginsCost}</h1>
+            <h1 className={styles.title}>Total: ${programsCost + pluginsCost}</h1>
+            <h1 className={styles.title}>Programs: ${programsCost}</h1>
+            <h1 className={styles.title}>Plugins: ${pluginsCost}</h1>
         </Card>
     )
 }
