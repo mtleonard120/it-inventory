1b966473e6f2eb6d265f01611b38bf6db3ca99e1
AlexMcDowell <alexmichaelmcdowell@gmail.com>
2019-06-12 16:11:00 -0400

    wip

--- src/frontend-admin/src/components/App/App.tsx
+++ src/frontend-admin/src/components/App/App.tsx
@@ -1,18 +1,5 @@
-<<<<<<< HEAD
 import React from 'react';
 import {Login} from '../reusables/Login/Login';
-=======
-import React from "react";
-import {
-  Route,
-  NavLink,
-  BrowserRouter as Router,
-  Switch
-} from "react-router-dom";
-
-// Components
-import { DashboardPage } from "../pages/DashboardPage/DashboardPage";
->>>>>>> f0045c282dbba8861886f70f3f8cf390e2043b4a
 
 // Styles
 import styles from "./App.module.css";
@@ -21,75 +8,7 @@ import styles from "./App.module.css";
 export const App: React.FC = () => {
   return (
     <div className={styles.app}>
-<<<<<<< HEAD
       <Login />
   </div>
-=======
-      {/*header*/}
-
-      <Router>
-        <div className={styles.navContainer}>
-          <nav className={styles.navBar}>
-            <h1>CQL</h1>
-            <div className={styles.navEllipse} />
-            <div className={styles.navRectangle} />
-
-            <div className={styles.linkContainer}>
-              <NavLink
-                className={styles.navTab}
-                activeClassName={styles.active}
-                to="/departments"
-              >
-                <div className={styles.navTabRectangle} />
-                <label>Departments</label>
-              </NavLink>
-
-              <NavLink
-                className={styles.navTab}
-                activeClassName={styles.active}
-                to="/hardware"
-              >
-                <div className={styles.navTabRectangle} />
-                <label>Hardware</label>
-              </NavLink>
-
-              <NavLink
-                className={styles.navTab}
-                activeClassName={styles.active}
-                to="/programs"
-              >
-                <div className={styles.navTabRectangle} />
-                <label>Programs</label>
-              </NavLink>
-
-              <NavLink
-                className={styles.navTab}
-                activeClassName={styles.active}
-                to="/employees"
-              >
-                <div className={styles.navTabRectangle} />
-                <label>Employees</label>
-              </NavLink>
-
-              <NavLink
-                className={styles.navTab}
-                activeClassName={styles.active}
-                to="/dashboard"
-              >
-                <div className={styles.navTabRectangle} />
-                <label>Dashboard</label>
-              </NavLink>
-            </div>
-          </nav>
-        </div>
-
-        <Switch>
-          {/* <Route />'s go here */}
-          <Route path="/dashboard" component={DashboardPage} />
-        </Switch>
-      </Router>
-      <footer className={styles.appFooter} />
-    </div>
->>>>>>> f0045c282dbba8861886f70f3f8cf390e2043b4a
   );
 };
