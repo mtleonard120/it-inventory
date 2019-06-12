e8263b160d827a022ad0f9647c5e1f1f56fdb86f
AlexMcDowell <alexmichaelmcdowell@gmail.com>
2019-06-12 14:25:26 -0400

    fix

--- src/frontend-admin/src/components/reusables/Card/Card.tsx
+++ src/frontend-admin/src/components/reusables/Card/Card.tsx
@@ -1,22 +1,24 @@
-import React from 'react'
-import styles from './Card.module.css'
-import {Title} from '../Title/Title' 
-import cx from 'classnames'
+import React from "react";
+import styles from "./Card.module.css";
+import { Title } from "../Title/Title";
+import { concatStyles } from "../../../utilities/mikesConcat";
 
 interface ICardProps {
-    title?: any
-    children: any //any so that dropdown can be used
-    className?: string
-    titleClassName?: string
+  title?: any;
+  children: any; //any so that dropdown can be used
+  className?: string;
+  titleClassName?: string;
 }
 
-export const Card: React.FC<ICardProps> = (props) => {
-    const { title, children, className, titleClassName } = props
+export const Card: React.FC<ICardProps> = props => {
+  const { title, children, className, titleClassName } = props;
 
-    return (
-        <div className={styles.cardMain}>
-            {title && <Title title={title} className={titleClassName}/>}
-            <div className={cx(styles.cardChildren, className)}>{children}</div>
-        </div>
-    )
-}
+  return (
+    <div className={styles.cardMain}>
+      {title && <Title title={title} className={titleClassName} />}
+      <div className={concatStyles(styles.cardChildren, className)}>
+        {children}
+      </div>
+    </div>
+  );
+};
--- src/frontend-admin/src/components/reusables/Title/Title.tsx
+++ src/frontend-admin/src/components/reusables/Title/Title.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import styles from './Title.module.css' 
-import cx from 'classnames'
+import React from "react";
+import styles from "./Title.module.css";
+import { concatStyles } from "../../../utilities/mikesConcat";
 
 interface ITitleProps {
-    title: any //any so that dropdown can be used
-    className?: string
+  title: any; //any so that dropdown can be used
+  className?: string;
 }
 
-export const Title: React.FC<ITitleProps> = (props) => {
-    const { title, className } = props
+export const Title: React.FC<ITitleProps> = props => {
+  const { title, className } = props;
 
-    return (
-        <div className={cx(styles.titleContainer, className)}>
-            <div className={styles.bottomCorner}/>
-                <div className={styles.title}>{title}</div>
-            <div className={styles.topCorner}/>
-        </div>            
-    )
-}
+  return (
+    <div className={concatStyles(styles.titleContainer, className)}>
+      <div className={styles.bottomCorner} />
+      <div className={styles.title}>{title}</div>
+      <div className={styles.topCorner} />
+    </div>
+  );
+};
