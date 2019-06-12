458bbcf86c33016b2127687e8e25251c854a9313
Kirstin Burns <35663199+kirbur@users.noreply.github.com>
2019-06-12 09:02:40 -0400

    IIWA-83 Created BarChartCard to place on dashboard

---
+++ src/frontend-admin/src/components/reusables/HorizontalBarChart/BarChartCard.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+
+// Components
+import { Card } from "../../reusables/Card/Card";
+import { Group } from "../../reusables/Group/Group";
+import { HorizontalBarChart } from "../../reusables/HorizontalBarChart/HorizontalBarChart";
+import { AxiosService } from "../../../services/AxiosService/AxiosService";
+
+// Primary Component
+export const BarChartCard: React.SFC<{ className: string }> = props => {
+  const { className } = props;
+
+  const axios = new AxiosService("accessToken", "refreshToken");
+  const list: {
+    programName: string;
+    CountProgInUse: number;
+    CountProgOverall: number;
+  }[] = [];
+  const [licenses, setLicenses] = useState(list);
+
+  useEffect(() => {
+    //TODO: find out endpoint name
+    axios.get("/Licenses", setLicenses);
+  }, [setLicenses]);
+
+  return (
+    <div className={className}>
+      <Card title={"licenses"}>
+        <Group>
+          {licenses.map(i => (
+            <HorizontalBarChart
+              title={i.programName}
+              amount={i.CountProgInUse}
+              outOf={i.CountProgOverall}
+              onClick={() => {}}
+            />
+          ))}
+        </Group>
+      </Card>
+    </div>
+  );
+};
