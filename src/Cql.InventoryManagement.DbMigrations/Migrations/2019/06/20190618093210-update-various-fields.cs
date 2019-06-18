ca6a221f00e7d281ebcee5465456bb9207bc4e69
Charles Kornoelje <charles.kornoelje@cqlcorp.com>
2019-06-18 10:03:11 -0400

    IIWA-100: Remove redundant columns and add helpful date columns.

---
+++ src/Cql.InventoryManagement.DbMigrations/Migrations/2019/06/20190618093210-update-various-fields.cs
@@ -0,0 +1,39 @@
+﻿using FluentMigrator;
+using System;
+
+namespace Cql.InventoryManagement.DbMigrations.Migrations._2019._06
+{
+    [Migration(20190618093210)]
+    public class _20190618093210_update_various_fields : AutoReversingMigration
+    {
+        public override void Up()
+        {
+            Rename.Column("ProgramCostPerEmployee")
+                .OnTable("Program")
+                .To("ProgramFlatCost");
+            Alter.Table("Program")
+                .AddColumn("RenewalDate")
+                .AsDate()
+                .Nullable();
+            Alter.Table("Program")
+                .AddColumn("MonthsPerRenewal")
+                .AsInt32()
+                .Nullable();
+            Alter.Table("ProgramHistory")
+                .AddColumn("EventDate")
+                .AsDateTime()
+                .NotNullable()
+                .WithDefaultValue(1800);
+            Alter.Table("HardwareHistory")
+                .AddColumn("EventDate")
+                .AsDateTime()
+                .NotNullable()
+                .WithDefaultValue(1800);
+            Alter.Table("Plugins")
+                .AddColumn("ProgramName")
+                .AsString()
+                .Nullable();
+        }
+    }
+}
+
---
+++ src/Cql.InventoryManagement.DbMigrations/Migrations/2019/06/20190618095810-remove-redundant-columns.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+using System;
+
+namespace Cql.InventoryManagement.DbMigrations.Migrations._2019._06
+{
+    [Migration(20190618095810)]
+    public class _20190618095810_remove_redundant_columns : Migration
+    {
+        public override void Up()
+        {
+            Delete.Column("ComputerName")
+                .FromTable("Computer");
+            Delete.Column("ServerName")
+                .FromTable("Server");
+        }
+
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
