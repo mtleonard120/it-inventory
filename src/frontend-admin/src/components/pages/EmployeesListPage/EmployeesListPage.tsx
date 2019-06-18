1e8d8313f56aedcfd2140a11bd75e8cc8fb37355
Kirstin Burns <kirstin.burns@cqlcorp.com>
2019-06-18 14:51:40 -0400

    IIWA-99 Created search w/ filter

--- src/frontend-admin/src/components/pages/EmployeesListPage/EmployeesListPage.tsx
+++ src/frontend-admin/src/components/pages/EmployeesListPage/EmployeesListPage.tsx
@@ -1,22 +1,61 @@
-import React from "react";
+import React, {useState, useEffect} from 'react'
 
 // Packages
 
 // Components
+import {FilteredSearch, IFilteredSearchProps} from '../../reusables/FilteredSearch/FilteredSearch'
 
 // Utils
 
 // Styles
-import styles from "./EmployeesListPage.module.css";
+import styles from './EmployeesListPage.module.css'
 
 // Types
 interface IEmployeesListPageProps {}
 
-// Helpers
+const initListData: any[] = []
 
 // Primary Component
 export const EmployeesListPage: React.SFC<IEmployeesListPageProps> = props => {
-  const {} = props;
+    const {} = props
 
-  return <div>This is the employee list page.</div>;
-};
+    const [listData, setListData] = useState(initListData)
+    const [filtered, setFiltered] = useState(listData)
+    const [search, setSearch] = useState('')
+    const [selected, setSelected] = useState({label: 'name', value: 'name'})
+
+    useEffect(() => {
+        let filteredTableInput = listData
+        filteredTableInput = listData.filter((row: any) => {
+            return (
+                row[selected.value]
+                    .toString()
+                    .toLowerCase()
+                    .search(search.toLowerCase()) !== -1
+            )
+        })
+        setFiltered(filteredTableInput)
+    }, [search, selected])
+
+    return (
+        <div className={styles.employeesListMain}>
+            {/*<AddButton />*/}
+
+            <FilteredSearch
+                search={search}
+                setSearch={setSearch}
+                options={[
+                    {label: 'name', value: 'name'},
+                    {label: 'date Hired', value: 'dateHired'},
+                    {label: 'days Employed', value: 'daysEmployed'},
+                    {label: 'cost', value: 'cost'},
+                ]} //TODO: get real column names
+                selected={selected}
+                setSelected={setSelected}
+                className={styles.search}
+            />
+
+            {/*<List />*/}
+        </div>
+    )
+}
